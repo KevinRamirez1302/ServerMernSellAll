116cf1a60ea104aeefe36a52ae4c9ce3b077511d
Kevin <kevin.alexanderra1302@gmail.com>
2025-10-04 13:18:20 +0100

    fix-secretkey

--- Middlewares/validateToken.js
+++ Middlewares/validateToken.js
@@ -1,12 +1,14 @@
 import { secretkey } from '../config.js';
 import jwt from 'jsonwebtoken';
 
+const logicKey = secretkey || process.env.secretkey;
+
 export const authRequired = (req, res, next) => {
   const { token } = req.cookies;
 
   if (!token) return res.status(401).json({ message: 'Not token unathorized' });
 
-  jwt.verify(token, secretkey, (err, user) => {
+  jwt.verify(token, logicKey, (err, user) => {
     if (err) return res.status(400).json({ message: 'Token error' });
 
     req.user = user;
